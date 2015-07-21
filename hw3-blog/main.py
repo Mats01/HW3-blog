678548b72517189a3b130a8c754e1ea1399b1bc3
Mats <matej.butkovic@gmail.com>
2015-07-21 15:47:25 +0200

    Autentification fully working

--- hw3-blog/main.py
+++ hw3-blog/main.py
@@ -22,24 +22,50 @@ import jinja2
 import re
 import hashlib
 import hmac
+import random
+import string
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
 
-
 SECRET = 'imsosecret'
+
+
 def hash_str(s):
     return hmac.new(SECRET, s).hexdigest()
 
+
 def make_secure_val(s):
     return "%s|%s" % (s, hash_str(s))
+
 
 def check_secure_val(h):
     val = h.split('|')[0]
     if h == make_secure_val(val):
         return val
+
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in range(5))
+
+
+salt = ''
+
+
+def make_pw_hash(pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(pw + salt).hexdigest()
+    return '%s|%s' % (h, salt)
+
+
+def valid_pw(pw, h):
+    salt = h.split('|')[1]
+    if h == make_pw_hash(pw, salt=salt):
+        return h
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -80,11 +106,11 @@ class BlogPost(db.Model):
         self._render_text = self.content.replace('\n', '<br>')
         return render_str("post1.html", p=self)
 
+
 class User(db.Model):
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.EmailProperty(required=False)
-
 
 
 class Handler(webapp2.RequestHandler):
@@ -100,13 +126,20 @@ class Handler(webapp2.RequestHandler):
 
 
 class MainHandler(Handler):
-    def render_post(self, posts):
+    def render_post(self, posts, hidden):
         posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC")
 
-        self.render('blogs.html', posts=posts)
+        self.render('blogs.html', posts=posts, hidden=hidden)
 
     def get(self):
-        self.render_post("")
+        user_id = self.request.cookies.get('user_id')
+        if user_id:
+            id = check_secure_val(user_id)
+            key = db.Key.from_path('User', int(id))
+            if key:
+                self.render_post("", "")
+        else:
+            self.render_post(posts=None, hidden='none')
 
 
 class PostHandler(Handler):
@@ -114,7 +147,15 @@ class PostHandler(Handler):
         self.render('post.html', subject=subject, content=content, error=error)
 
     def get(self):
-        self.render('post.html')
+        user_id = self.request.cookies.get('user_id')
+        if user_id:
+            id = check_secure_val(user_id)
+            key = db.Key.from_path('User', int(id))
+            if key:
+                self.render('post.html')
+        else:
+            self.redirect('/blog/login')
+
 
     def post(self):
         subject = self.request.get('subject')
@@ -164,6 +205,10 @@ class SingUpHandler(Handler):
         email_error = ''
         input_email = self.request.get('email')
         username_exsists = ''
+        users = db.GqlQuery("SELECT * FROM User")
+        for i in users:
+            if i.username == input_username:
+                username_exsists='That username already exsists!'
         if self.request.get('email'):
             if not email:
                 check = True
@@ -196,29 +241,72 @@ class SingUpHandler(Handler):
         else:
             if check:
                 self.re_render(username=input_username, email=input_email, email_error=email_error,
-                               username_error='', password_error='', verification_error='', username_exsists=username_exsists)
+                               username_error='', password_error='', verification_error='',
+                               username_exsists=username_exsists)
+            elif username_exsists:
+                self.re_render(username=input_username, email=input_email, email_error=email_error,
+                               username_error='', password_error='', verification_error='',
+                               username_exsists=username_exsists)
             else:
                 if not input_email:
                     input_email = None
-                u = User(username=input_username, password=verify_password, email=input_email)
+                u = User(username=input_username, password=make_pw_hash(verify_password), email=input_email)
                 u.put()
                 new_cookie = make_secure_val(str(u.key().id()))
                 self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % new_cookie)
                 self.redirect('/blog/welcome')
 
+
+
 class WelcomeHandler(Handler):
     def get(self):
         user_id = self.request.cookies.get('user_id')
-        id = check_secure_val(user_id)
-        key = db.Key.from_path('User', int(id))
-        if key:
-            user = db.get(key)
-            self.write('Welcome, %s!' % user.username)
+        if user_id:
+            id = check_secure_val(user_id)
+            key = db.Key.from_path('User', int(id))
+            if key:
+                user = db.get(key)
+                self.write('Welcome, %s!' % user.username)
         else:
             self.redirect('/blog/signup')
+
+class LogInHandler(Handler):
+    def render_error(self, error):
+        self.render('login.html', error=error)
+    def get(self):
+        self.render('login.html')
+
+    def post(self):
+        input_username = self.request.get('username')
+        user_pw = self.request.get('password')
+        users = db.GqlQuery("SELECT * FROM User")
+        nr_users = 0
+        nr_loops = 0
+        for i in users:
+            nr_users = nr_users + 1
+        for i in users:
+            nr_loops = nr_loops + 1
+            if i.username == input_username:
+                s_pw = i.password
+                s = s_pw.split('|')[1]
+                h = make_pw_hash(user_pw)
+                if valid_pw(user_pw, h):
+                    if make_pw_hash(user_pw, s) == i.password:
+                        new_cookie = make_secure_val(str(i.key().id()))
+                        self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % new_cookie)
+                        self.redirect('/blog/welcome')
+            elif nr_users == nr_loops:
+                self.render_error(error='Invalid username or password!')
+
+class LogOutHandler(Handler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+        self.redirect('/blog/signup')
+
 
 
 app = webapp2.WSGIApplication([
     ('/blog', MainHandler), ('/blog/newpost', PostHandler), ('/blog/([0-9]+)', PostPage),
-    ('/blog/signup', SingUpHandler), ('/blog/welcome', WelcomeHandler)
+    ('/blog/signup', SingUpHandler), ('/blog/welcome', WelcomeHandler), ('/blog/login', LogInHandler),
+    ('/blog/logout', LogOutHandler)
 ], debug=True)
